729cc61c6d768f2d9535e1a6c2cc2e69c7b3e74c
Adam Kopuletý <zerostar362@live.com>
2021-05-11 00:45:04 +0200

    Fixed player object creation. Updated render. bit of a graphics added. General object created. Should ease the code from now on.

--- ConssoleApp/Program.cs
+++ ConssoleApp/Program.cs
@@ -6,21 +6,139 @@ using System.Threading.Tasks;
 
 namespace ConssoleApp
 {
+
     class Program
     {
-        public bool WinningCondition = false;
 
-        public void SetWinner()
+        class Game
         {
+            private int number;
 
+            public bool WinningCondition = false;
+
+            public void SetWinner()
+            {
+
+            }
+
+            public void CreatePlayers(int Count,out object player1,out object player2, out object player3, out object player4)
+            {
+                player1 = null;
+                player2 = null;
+                player3 = null;
+                player4 = null;
+
+                for (int i = 0; i <= Count - 1; i++)
+                {
+                    string index = i.ToString();
+
+                    switch (i)
+                    {
+                        case 0:
+                            {
+                                Console.WriteLine("Choose player name");
+                                Player playerOne = new Player(Console.ReadLine(), i);
+                                player1 = playerOne;
+                                break;
+                            }
+                        case 1:
+                            {
+                                Console.WriteLine("Choose player name");
+                                Player playerTwo = new Player(Console.ReadLine(), i);
+                                player2 = playerTwo;
+                                break;
+                            }
+                        case 2:
+                            {
+                                Console.WriteLine("Choose player name");
+                                Player playerThree = new Player(Console.ReadLine(), i);
+                                player3 = playerThree;
+                                break;
+                            }
+                        case 3:
+                            {
+                                Console.WriteLine("Choose player name");
+                                Player playerFour = new Player(Console.ReadLine(), i);
+                                player4 = playerFour;
+                                break;
+                            }
+                        default:
+                            {
+                                Console.WriteLine("Ooops, Something went wrong");
+                                break;
+                            }
+                    }
+                }
+            }
+
+            public int CheckValidInt(string PlayerCount)
+            {
+                bool validNumber = false;
+
+                while (validNumber == false)
+                {
+                    try
+                    {
+                        number = Int32.Parse(PlayerCount);
+                        validNumber = true;
+                        return number;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Not a valid input");
+                        Console.WriteLine("Enter a valid number");
+                        PlayerCount = Console.ReadLine();
+                    }
+                }
+
+                if (number > 0)
+                {
+                    return number;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
+            public void ClearConsole()
+            {
+                Console.Clear();
+            }
+
+            public void WelcomeMessage()
+            {
+                Console.WriteLine("Welcome to the game");
+            }
+
+            public void OverHead()
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("Snakes And Ladders");
+                Console.WriteLine(" ");
+                Console.WriteLine("################################################################");
+                Console.WriteLine(" ");
+            }
+
+            public void Footer()
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("################################################################");
+                Console.WriteLine(" ");
+            }
+
+            public int setPlayerTurn()
+            {
+                return 0;
+            }
         }
 
         class Player
         {
-            public int X;
-            public int Y;
-            private String color;
-            private String name;
+            public int X { get; set; }
+            public int Y { get; set; }
+            private String color { get; }
+            private String name { get; }
 
             public Player(String PlayerName, int PlayerColor)
             {
@@ -44,11 +162,6 @@ namespace ConssoleApp
             {
                 X = 0;
                 Y = 0;
-            }
-
-            public string getColor()
-            {
-                return color;
             }
 
             public void movePlayer(int movementValue, out bool Won)
@@ -120,30 +233,23 @@ namespace ConssoleApp
         static void Main(string[] args)
         {
 
-            bool validNumber = false;
-            int NumberOfPlayers = 0;
-
             Console.Title = "Snakes and ladders";
             board GameBoard = new board();
+            Game General = new Game();
+            Dice dice = new Dice();
 
+            //this block creates all wanted player objects-----------------------------------------
             Console.WriteLine("How many players will play ?");
-            
-            while (validNumber == false)
-            {
-                try
-                {
-                    NumberOfPlayers = Int32.Parse(Console.ReadLine());
-                    validNumber = true;
-                }
-                catch
-                {
-                    Console.WriteLine("Not a valid input");
-                    validNumber = false;
-                }
-            }
-            for(int i = 0; i <= NumberOfPlayers; i++)
-            {
+                General.CreatePlayers(General.CheckValidInt(Console.ReadLine()), out object playerOne, out object playerTwo, out object playerThree, out object playerFour);
+            //-------------------------------------------------------------------------------------
 
+            while(General.WinningCondition == false)
+            {
+                General.ClearConsole();
+                General.OverHead();
+                GameBoard.Render();
+                General.Footer();
+                Console.ReadLine();
             }
         }
     }
